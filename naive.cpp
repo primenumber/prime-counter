2d0755746c2ed2a43087b44c355203c1576140f2
prime <prime@kmc.gr.jp>
2015-06-18 04:48:12 +0900

    impl naive sieve of eratosthenes

---
+++ naive.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include "naive.hpp"
+
+namespace naive {
+
+result::result prime_count(const uint64_t n) {
+  std::vector<bool> is_prime(n+1, true);
+  is_prime[0] = is_prime[1] = false;
+  uint64_t sqrt_n = sqrt(n+1);
+  for (uint64_t i = 2; i <= sqrt_n; ++i)
+    if (is_prime[i])
+      for (uint64_t j = i*i; j <= n; j += i)
+        is_prime[j] = false;
+  result::result res(n, sqrt_n);
+  for (uint64_t i = 1; i <= n; ++i)
+    res.count[(i-1)/sqrt_n] += is_prime[i];
+  return res;
+}
+
+} // namespace naive
---
+++ naive.hpp
@@ -0,0 +1,7 @@
+#include "result.hpp"
+
+namespace naive {
+
+result::result prime_count(const uint64_t n);
+
+} // namespace naive
