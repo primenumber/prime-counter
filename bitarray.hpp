b53c6a9e1f76e801c759adc15567c68893e93660
prime <prime@kmc.gr.jp>
2015-09-05 05:27:08 +0900

    fix bug bitarray::count

--- bitarray.hpp
+++ bitarray.hpp
@@ -38,8 +38,13 @@ class bitarray {
   }
   std::size_t count(std::size_t first, std::size_t last) const {
     std::size_t sum = 0;
-    sum += popcount(data[lower_index(first)] & (~0 << first & 0x3F));
-    sum += popcount(data[lower_index(last)] & (~0 >> (63-(first & 0x3F))));
+    if (lower_index(first) != upper_index(first)) {
+      sum += popcount(data[lower_index(first)]
+          & (~UINT64_C(0) << (first & 0x3F)));
+    } if (lower_index(last) != upper_index(last)) {
+      sum += popcount(data[lower_index(last)]
+          & (~UINT64_C(0) >> (64-(last & 0x3F))));
+    }
     for (std::size_t i = upper_index(first); i < lower_index(last); ++i) {
       sum += popcount(data[i]);
     }
@@ -61,12 +66,8 @@ class bitarray {
  private:
   static std::size_t upper_index(std::size_t n) { return (n+63) >> 6; }
   static std::size_t lower_index(std::size_t n) { return n >> 6; }
-  static std::size_t ceil_64(std::size_t n) {
-    return (n & 0x3F) ? ((n | 0x3F) + 1) : n;
-  }
-  static std::size_t floor_64(std::size_t n) { return n & ~0x3F; }
   static std::size_t popcount(uint64_t bits) {
-    return __builtin_popcount(bits);
+    return __builtin_popcountll(bits);
   }
   std::vector<uint64_t> data;
   std::size_t sz;
